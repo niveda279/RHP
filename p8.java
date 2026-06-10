668b836b66c784f415973269f2068ab82fbbeae0
NIVEDA EZHILARASU <24110.cs@rmkcet.ac.in>
2026-06-10 19:19:22 +0530

    Add files via upload

---
+++ p8.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s1 = sc.next();
+        String s2 = sc.next();
+
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Build DP table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // Reconstruct LCS
+        StringBuilder lcs = new StringBuilder();
+
+        int i = n, j = m;
+
+        while (i > 0 && j > 0) {
+
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                lcs.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] >= dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        lcs.reverse();
+
+        System.out.println("Length = " + dp[n][m]);
+        System.out.println("LCS = " + lcs);
+    }
+}
