8af2159bc2562e11455b89c9edacc41150ddd711
NIVEDA EZHILARASU <24110.cs@rmkcet.ac.in>
2026-06-11 11:14:18 +0530

    Update p9.java

--- p9.java
+++ p9.java
@@ -1,33 +1,26 @@
-import java.util.HashMap;
 import java.util.*;
-public class EqualZeroOneSubstrings {
+public class EqualZeroOneDP {
     public static int countSubstrings(String s) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        int balance = 0;
-        int count = 0;
-
-        map.put(0, 1);
-
-        for (char ch : s.toCharArray()) {
-
-            if (ch == '1')
-                balance += 1;
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (s.charAt(i - 1) == '1')
+                dp[i] = dp[i - 1] + 1;
             else
-                balance -= 1;
-
-            if (map.containsKey(balance))
-                count += map.get(balance);
-
-            map.put(balance, map.getOrDefault(balance, 0) + 1);
+                dp[i] = dp[i - 1] - 1;
         }
-
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (dp[j] == dp[i])
+                    count++;
+            }
+        }
         return count;
     }
-
     public static void main(String[] args) {
     	Scanner sc=new Scanner(System.in);
-        String s = sc.nextLine() ;
+        String s = sc.nextLine();
         System.out.println(countSubstrings(s));
     }
 }
